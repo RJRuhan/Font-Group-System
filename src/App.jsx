8886fe98ef6e808ed272b37a2c32882c67ce7ca0
Mac <mac@Macs-MacBook-Air.local>
2025-04-01 16:11:40 +0600

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import { FontProvider } from './context/FontContext';
+import FontUploader from './components/FontUploader';
+import FontList from './components/FontList';
+import FontGroupForm from './components/FontGroupForm';
+import FontGroupList from './components/FontGroupList';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+    
+    
+    // <FontProvider>
+    //   <div className="container mx-auto px-4 py-8 max-w-4xl">
+    //     <h1 className="text-2xl font-bold mb-6 text-center">Font Group Management System</h1>
+        
+    //     <div className="bg-gray-50 p-6 rounded-lg shadow-sm">
+    //       <FontUploader />
+    //       <FontList />
+    //       <FontGroupForm />
+    //       <FontGroupList />
+    //     </div>
+    //   </div>
+    // </FontProvider>
+    
+  );
+}
+
+export default App;
